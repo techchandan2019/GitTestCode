132ee201b078fc433aa91f8c88a22348719aa780
tech.chandan2019 <tech.chandan2019@gmail.com>
2024-03-14 10:51:05 +0530

    3nd commit in branch

--- src/main/java/com/bonami/controller/TestController.java
+++ src/main/java/com/bonami/controller/TestController.java
@@ -1,5 +1,13 @@
 package com.bonami.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
 
+	@GetMapping("/msg")
+	public String getMessage() {
+		return "Hi";
+	}
 }
